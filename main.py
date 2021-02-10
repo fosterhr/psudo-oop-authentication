e0a3afddeaedd3ddd00fa32b4ca4b362977046e0
fosterhr <74639218+fosterhr@users.noreply.github.com>
2021-02-10 16:56:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-__title__ = "Psudo OOP Authentication"
+__title__ = "Pseudo OOP Authentication"
 __description__ = "This program creates a user based on given input, 'encrypts' the password, and then provides the user with account information."
 
 __author__ = "Foster Reichert"
@@ -11,56 +11,56 @@ abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"
 # define user class
 # used to store and return information about the user
 class User:
-	def __init__(self, email, username, password):
-		self.email = email
-		self.username = username
-		self.password = password
-	
-	def init(self):
-    	# get length of password
-		self.password_length = len(self.password)
-    	# create a dynamic "*" placeholder
-		self.password_placeholder = "*" * self.password_length
-    	# "encrypt" password using ord() and the abc list
-		temp = self.password
-		self.password = ""
-		for i in range(len(temp)):
-			self.password += str(ord(temp[i]))
-		temp = self.password
-		self.password = ""
-		for i in range(len(temp)):
-			self.password += abc[int(temp[i])]
-			
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password = password
+        self.load()
+    
+    def load(self):
+        # get length of password
+        self.password_length = len(self.password)
+        # create a dynamic "*" placeholder
+        self.password_placeholder = "*" * self.password_length
+        # "encrypt" password using ord() and the abc list
+        temp = self.password
+        self.password = ""
+        for i in range(len(temp)):
+            self.password += str(ord(temp[i]))
+        temp = self.password
+        self.password = ""
+        for i in range(len(temp)):
+            self.password += abc[int(temp[i])]
+            
 def get_email():
-	email = input("What is your email? ")
-	if email == "" or None:
-		print("Missing required field (email)")
-		print("Program will now exit.")
-		exit()
-	else:
-		return email
+    email = input("What is your email? ")
+    if email == "" or None:
+        print("Missing required field (email)")
+        print("Program will now exit.")
+        exit()
+    else:
+        return email
 
 def get_username():
-	username = input("Please enter a username: ")
-	if username == "" or None:
-		print("Missing required field (username)")
-		print("Program will now exit.")
-		exit()
-	else:
-		return username
+    username = input("Please enter a username: ")
+    if username == "" or None:
+        print("Missing required field (username)")
+        print("Program will now exit.")
+        exit()
+    else:
+        return username
 
 def get_password():
-	password = input("Choose a password: ")
-	if password == "" or None:
-		print("Missing required field (password)")
-		print("Program will now exit.")
-		exit()
-	else:
-		return password
+    password = input("Choose a password: ")
+    if password == "" or None:
+        print("Missing required field (password)")
+        print("Program will now exit.")
+        exit()
+    else:
+        return password
 
-# initilize user variables
+# initialize user variables
 user = User(get_email(), get_username(), get_password())
-user.init()
 
 bar = "---------------"
 for i in range(len(user.email)): bar += "-"
